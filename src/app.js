21e4b9a72f6dc0aafe578527444f6b1c7c1e780f
DANIELA <danielamiritescu@gmail.com>
2019-08-05 16:44:25 +0100

    initial commit MVP

---
+++ src/app.js
@@ -0,0 +1,31 @@
+import Vue from 'vue';
+
+document.addEventListener('DOMContentLoaded', () => {
+  new Vue({
+    el: "#app",
+    data: {
+      items:  [
+        {name: 'Buy_groceries', done: true},
+        {name: 'Clean_house', done: false},
+        {name: 'Book_yoga_session', done: true},
+        {name: 'Pay_tuition_fee', done: false}
+      ],
+        newItem: ''
+    },
+
+    methods: {
+      saveNewItem: function (e) {
+      e.preventDefault();
+      this.item.push({
+        name: this.newItem,
+        done: false
+      });
+      this.newItem = ''
+    },
+    doItem: function (index) {
+      this.items(index).done = true;
+      }
+    }
+  }
+)
+})
